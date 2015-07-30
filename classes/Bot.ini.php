d00508918825d09bfcedd87dbe443adf5b515d4f
Marco Napetti <marco.napetti@gmail.com>
2015-07-30 16:05:11 +0200

    Example ini file

---
+++ classes/Bot.ini.php
@@ -0,0 +1,7 @@
+;<?php /*
+;You have to create a file like this for every Bot Class
+;The php tags keep this file secure from web views
+HTTP_TOKEN = "<insert bot token here>";
+SECRET = "<a string to insert inside webhook to increase transmissions security>";
+DEVELOPER_CHAT_ID = "<your personal chat id, to receive suggestions>";
+;*/ ?>
